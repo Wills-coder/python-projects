9809358903a980226f949b011bef1d2352a1130b
Wills <linceaguayo@gmail.com>
2024-11-06 19:20:58 -0400

    Create Operaciones de asignación.py

---
+++ Operaciones de asignación.py
@@ -0,0 +1,21 @@
+name = "Hola "
+name += input("Escribe tu nombre: ")
+print(name, "este es el incremento y decremento de una variable \n")
+print("Incremento o aumento:\n")
+x = 1
+print("El valor inicial de x es:", x)
+x+=1
+x+=1
+x+=1
+x+=1
+print("El valor final de x es", x, "\n")
+print("decremento o disminución:\n")
+x = 5
+print("El valor inicial de x es:",x )
+x-=1
+x-=1
+x-=1
+x-=1
+print("El valor final de x es:", x,"\n" )
+
+'''estos son solo simples apuntes puestos en práctica.'''
